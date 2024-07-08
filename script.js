c8f4a7c2b4e8d40ef394319e269a6269868f04a1
David Bielik <davidbielik@gmail.com>
2024-07-08 17:07:25 -0400

    changed styling

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const topDiv = document.getElementById("top");
+const topTab = document.getElementById("top-tab");
+const bottomTopTab = document.getElementById("bottom-top-tab");
+const vidDiv = document.getElementById("videos");
+const vidTab = document.getElementById("videos-tab");
+const bottomVidTab = document.getElementById("bottom-videos-tab");
 const calDiv = document.getElementById("calendar");
 const calTab = document.getElementById("calendar-tab");
+const bottomCalTab = document.getElementById("bottom-calendar-tab");
 const aboutDiv = document.getElementById("about");
 const aboutTab = document.getElementById("about-tab");
-const contDiv = document.getElementById("contact");
+const bottomAboutTab = document.getElementById("bottom-about-tab");
+const contDiv = document.getElementById("footer");
 const contTab = document.getElementById("contact-tab");
+
 
 function scroll(div, tab) {
 tab.addEventListener("click", () => {
@@ -11,6 +20,12 @@ tab.addEventListener("click", () => {
 });
 }
 
-scroll(calDiv, calTab)
-scroll(aboutDiv, aboutTab)
-scroll(contDiv, contTab)
+scroll(calDiv, calTab);
+scroll(aboutDiv, aboutTab);
+scroll(contDiv, contTab);
+scroll(topDiv, topTab);
+scroll(vidDiv, vidTab);
+scroll(calDiv, bottomCalTab);
+scroll(aboutDiv, bottomAboutTab);
+scroll(topDiv, bottomTopTab);
+scroll(vidDiv, bottomVidTab);
